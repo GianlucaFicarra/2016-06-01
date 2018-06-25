0345d93215e16d40c31746bd0af22b8b27105be8
Utente <Utente@Utente-pc>
2018-06-25 15:45:11 +0200

    master

--- src/it/polito/tdp/flight/Main.java
+++ src/it/polito/tdp/flight/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 			
 			FlightController controller = loader.getController() ;
 			
+			
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
